0196dd1d3452c5d3e1d757beffe210b9af5ce5cd
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2022-05-05 19:05:56 +0300

    added app_assets

---
+++ lib/constants/app_assets.dart
@@ -0,0 +1,11 @@
+class AppAssets {
+  // Imgs
+  final profile_pic = 'lib/Assets/img/profile.png';
+
+  // Icons
+ final restart_icon = 'lib/Assets/icons/againicon.svg';
+ final edit_icon = 'lib/Assets/icons/editicon.svg';
+ final menu_icon = 'lib/Assets/icons/feather_menu.svg';
+ final pause_icon = 'lib/Assets/icons/pauseicon.svg';
+ final stop_icon = 'lib/Assets/icons/stopicon.svg';
+}
